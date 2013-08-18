613456485e4744796bcb7cbb49191e4120c9d97c
Alberto Solano <ajxweb571@gmail.com>
2013-08-18 22:27:49 +0200

    first commit - added main files
    added iterative/recursive binary search algorithms

---
+++ KataTwo-BinarySearch/BinarySearch.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTwo_BinarySearch
+{
+    public class BinarySearch
+    {
+        /// <summary>
+        /// Iterative binary search. It uses the for loops to find the given element.
+        /// </summary>
+        /// <param name="elementToFind">The element we want to find.</param>
+        /// <param name="inputArray">The array in which we want to find the given element.</param>
+        /// <returns></returns>
+        public int ChopIterative(int elementToFind, int[] inputArray)
+        {
+            int indexFound = -1;
+
+            for (int i = 0; i < (inputArray.Length / 2); i++)
+            {
+                if (inputArray[i] == elementToFind)
+                {
+                    indexFound = i;
+                    return indexFound;
+                }
+            }
+
+            for (int i = (inputArray.Length / 2); i < (inputArray.Length - 1); i++)
+            {
+                if (inputArray[i] == elementToFind)
+                {
+                    indexFound = i;
+                    return indexFound;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Recursive binary search. It checks, recursively, if the given element is in the left middle or right middle of given array.
+        /// </summary>
+        /// <param name="elementToFind">The element we want to find.</param>
+        /// <param name="inputArray">The array in which we want to find the given element.</param>
+        /// <returns></returns>
+        public int ChopRecursive(int elementToFind, int[] inputArray)
+        {
+            return ChopRecursiveMain(elementToFind, inputArray, 0, inputArray.Length - 1);
+        }
+
+        /// <summary>
+        /// Main recursive binary search algorithm.
+        /// </summary>
+        /// <param name="elementToFind">The element we want to find.</param>
+        /// <param name="inputArray">The array in which we want to find the given element.</param>
+        /// <param name="min">The minimum index of the given array for the search.</param>
+        /// <param name="max">The maximum index of the given array for the search.</param>
+        /// <returns></returns>
+        private int ChopRecursiveMain(int elementToFind, int[] inputArray, int min, int max)
+        {
+            //When the recursive search has ended, without finding the element both in the left or in the right middle of the array, we have this condition.
+            if (min > max)
+            {
+                return -1;
+            }
+            int indexMiddleElement = min + (max - min) / 2;
+
+            if (elementToFind < inputArray[indexMiddleElement])
+            {
+                return ChopRecursiveMain(elementToFind, inputArray, 0, indexMiddleElement - 1);
+            }
+            else if (elementToFind > inputArray[indexMiddleElement])
+            {
+                return ChopRecursiveMain(elementToFind, inputArray, indexMiddleElement + 1, inputArray.Length - 1);
+            }
+            else
+            {
+                return indexMiddleElement;
+            }
+        }
+    }
+}
---
+++ KataTwo-BinarySearch/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTwo_BinarySearch
+{
+    class Program
+    {
+        //As we know, the binary search algorithm requires a sorted array.
+        static int[] mySortedArray = { 9, 74, 874, 2290, 43073, 279343 };
+
+        /// <summary>
+        /// Main program method.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
+        static void Main(string[] args)
+        {
+            var binarySearch = new BinarySearch();
+            Console.WriteLine("We have the sorted array { 9, 74, 874, 2290, 43073, 279343 }");
+            Console.WriteLine();
+            Console.WriteLine("Iterative binary search starting..");
+            Console.WriteLine();
+            int elementToFind = 43073;
+            int indexFound = binarySearch.ChopIterative(elementToFind, mySortedArray);
+            Console.WriteLine(String.Format("The result of binary search for element {0} is the index {1} ",elementToFind,indexFound));
+            if(indexFound != -1)
+            {
+                Console.WriteLine(String.Format("Indeed the array at index {0} has {1}",indexFound,mySortedArray[indexFound]));
+            }
+            Console.WriteLine();
+            elementToFind = 9;
+            indexFound = binarySearch.ChopIterative(elementToFind, mySortedArray);
+            Console.WriteLine(String.Format("The result of binary search for element {0} is the index {1} ",elementToFind,indexFound));
+            if(indexFound != -1)
+            {
+                Console.WriteLine(String.Format("Indeed the array at index {0} has {1}",indexFound,mySortedArray[indexFound]));
+            }
+            Console.WriteLine();
+            elementToFind = 5;
+            Console.WriteLine("Trying to search a not existing element : " + elementToFind + " What the result will be?");
+            indexFound = binarySearch.ChopIterative(elementToFind, mySortedArray);
+            Console.WriteLine(String.Format("The result of binary search for {0} (not existing element) is {1}",elementToFind,indexFound));
+            Console.WriteLine();
+            Console.WriteLine("Iterative binary search ended.");
+            Console.WriteLine("---");
+            Console.WriteLine();
+            Console.WriteLine("Recursive binary search starting..");
+            Console.WriteLine();
+            elementToFind = 43073;
+            indexFound = binarySearch.ChopRecursive(elementToFind, mySortedArray);
+            Console.WriteLine(String.Format("The result of binary search for element {0} is the index {1} ", elementToFind, indexFound));
+            if (indexFound != -1)
+            {
+                Console.WriteLine(String.Format("Indeed the array at index {0} has {1}", indexFound, mySortedArray[indexFound]));
+            }
+            Console.WriteLine();
+            elementToFind = 9;
+            indexFound = binarySearch.ChopRecursive(elementToFind, mySortedArray);
+            Console.WriteLine(String.Format("The result of binary search for element {0} is the index {1} ", elementToFind, indexFound));
+            if (indexFound != -1)
+            {
+                Console.WriteLine(String.Format("Indeed the array at index {0} has {1}", indexFound, mySortedArray[indexFound]));
+            }
+            Console.WriteLine();
+            elementToFind = 789356;
+            Console.WriteLine("Trying to search a not existing element : " + elementToFind + " What the result will be?");
+            indexFound = binarySearch.ChopRecursive(elementToFind, mySortedArray);
+            Console.WriteLine(String.Format("The result of binary search for {0} (not existing element) is {1}", elementToFind, indexFound));
+            Console.WriteLine();
+            Console.WriteLine("Recursive binary search ended.");
+            Console.WriteLine("---");
+
+            Console.ReadKey();
+
+        }
+    }
+}
